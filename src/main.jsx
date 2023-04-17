aaceed3331222aaf3c761f135f4b9653b1647386
seeker11 <jmesabong@gmail.com>
2023-04-17 15:57:36 -0400

    Configuracion de las rutas del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,12 @@ import App from "./App";
 import "./index.css";
 import WebFont from "webfontloader";
 import Contenedor from "./elements/Contenedor";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InicioSesion from './components/InicioSesion';
+import EditarGasto from './components/EditarGasto';
+import GastosPorCategoria from './components/GastosPorCategoria';
+import ListaGastos from './components/ListaGastos';
+import RegistroUsuarios from './components/RegistroUsuarios';
 
 WebFont.load({
   google: {
@@ -14,8 +19,18 @@ WebFont.load({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Contenedor>
-      <App />
-    </Contenedor>
+    <BrowserRouter>
+      <Contenedor>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/iniciar-sesion" element={<InicioSesion />} />
+          <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
+          <Route path="/categorias" element={<GastosPorCategoria />} />
+          <Route path="/lista" element={<ListaGastos />} />
+          <Route path="/editar/:id" element={<EditarGasto />} />
+          <Route path="*" element={<App />} />
+        </Routes>
+      </Contenedor>
+    </BrowserRouter>
   </React.StrictMode>
 );
